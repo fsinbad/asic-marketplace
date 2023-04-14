75acaf850c976fb86acfa52cf2195c00ebf48bcd
Maximilien Fathi <maximilian.fathi@gmail.com>
2023-04-14 11:00:42 -0400

    Refactor code to dynamically manage products grid

---
+++ src/lib/payments.js
@@ -0,0 +1,3 @@
+export function initiateCheckout() {
+    console.log("Checkout!")
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,55 +2,54 @@ import Head from 'next/head'
 import Image from 'next/image'
 // import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
+import products from '@/products.json'
+import {initiateCheckout} from "@/lib/payments";
 
-// const inter = Inter({ subsets: ['latin'] })
+// const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
-  return (
-      <div className={styles.container}>
-        <Head>
-          <title>Create Next App</title>
-          <link rel="icon" href="/favicon.ico" />
-        </Head>
+    console.log('products', products)
+    return (
+        <div className={styles.container}>
+            <Head>
+                <title>Create Next App</title>
+                <link rel="icon" href="/favicon.ico"/>
+            </Head>
 
-        <main className={styles.main}>
-          <h1 className={styles.title}>
-            Android Marketplace
-          </h1>
+            <main className={styles.main}>
+                <h1 className={styles.title}>
+                    Android Marketplace
+                </h1>
 
-          <p className={styles.description}>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit!
-          </p>
+                <p className={styles.description}>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit!
+                </p>
 
-          <ul className={styles.grid}>
-            <li className={styles.card}>
-              <a href="#">
-                <img src="" alt="Android Phone 1" />
-                <h3>Android Phone 1</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-              </a>
-            </li>
+                <ul className={styles.grid}>
+                    {products.map(product => {
+                        const {id, title, price, description, image} = product;
+                        return <li key={id} className={styles.card}>
+                            <a href="#">
+                                <img src={image} alt={title}/>
+                                <h3>{title}</h3>
+                                <p>{price}</p>
+                                <p>{description}</p>
+                            </a>
+                            <p>
+                                <button className={styles.button} onClick={() => {
+                                    initiateCheckout()
+                                }}>Buy Now!
+                                </button>
+                            </p>
+                        </li>
+                    })}
 
-            <li className={styles.card}>
-              <a href="#">
-                <img src="" alt="Android Phone 2" />
-                <h3>Android Phone 2</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-              </a>
-            </li>
 
-            <li className={styles.card}>
-              <a href="#">
-                <img src="" alt="Android Phone 3" />
-                <h3>Android Phone 3</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-              </a>
-            </li>
-          </ul>
-        </main>
+                </ul>
+            </main>
 
-        <footer className={styles.footer}>
-        </footer>
-      </div>
-  )
+            <footer className={styles.footer}>
+            </footer>
+        </div>
+    )
 }
