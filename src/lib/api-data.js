2fa6cae0ae709241fb16ae2d47667ab5060f491e
ma-fath <ma_fath@live.concordia.ca>
2024-06-01 16:03:19 -0400

    Improve code clarity

--- src/lib/api-data.js
+++ src/lib/api-data.js
@@ -7,10 +7,6 @@ async function getDataFromApiUri() {
       process.env.NODE_ENV === "development"
         ? "http://localhost:3000/api/products"
         : "https://ma-fath-asic-marketplace.vercel.app/api/products";
-    // if (!process.env.NEXT_PUBLIC_API_URI) {
-    //   throw new Error('Invalid/Missing environment variable: "PUBLIC_API_URI"');
-    // }
-    // let res = await fetch(process.env.NEXT_PUBLIC_API_URI);
     let res = await fetch(uri);
     return await res.json();
   } catch (e) {
